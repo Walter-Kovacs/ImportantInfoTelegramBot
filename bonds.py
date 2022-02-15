b221675f57c6307a1b376e63c53c76f67f82107b
Walter-Kovacs <roma.baranov@gmail.com>
2022-02-15 22:15:07 +0300

    [WIP] first variant

---
+++ bonds.py
@@ -0,0 +1,170 @@
+from datetime import datetime
+from enum import Enum
+from typing import List
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import CallbackContext
+
+
+class InputStep(Enum):
+    NOMINAL = 1,
+    INTEREST_RATE = 2,
+    MATURITY_DATE = 3,
+    MARKET_PRICE = 4,
+    END = 5
+
+
+class UnknownInputStep(Exception):
+    pass
+
+
+class StepParam(Enum):
+    NEXT_STEP = 1,
+    MSG_HEADER = 2,
+    EDITING_PARAM_TITLE = 3
+
+
+_STEP_PARAMS = {
+    InputStep.NOMINAL: {
+        StepParam.NEXT_STEP: InputStep.INTEREST_RATE,
+        StepParam.MSG_HEADER: 'Введите номинал облигации',
+        StepParam.EDITING_PARAM_TITLE: 'Номинал'
+    },
+    InputStep.INTEREST_RATE: {
+        StepParam.NEXT_STEP: InputStep.MATURITY_DATE,
+        StepParam.MSG_HEADER: 'Введите купон в процентах',
+        StepParam.EDITING_PARAM_TITLE: 'Купон (%)'
+    },
+    InputStep.MATURITY_DATE: {
+        StepParam.NEXT_STEP: InputStep.MARKET_PRICE,
+        StepParam.MSG_HEADER: 'Введите дату погашения в формате дд.мм.гггг',
+        StepParam.EDITING_PARAM_TITLE: 'Дата погашения'
+    },
+    InputStep.MARKET_PRICE: {
+        StepParam.NEXT_STEP: InputStep.END,
+        StepParam.MSG_HEADER: 'Введите рыночную цену',
+        StepParam.EDITING_PARAM_TITLE: 'Цена'
+    }
+}
+
+
+def _get_next_step(step: InputStep) -> InputStep:
+    if step in _STEP_PARAMS:
+        return _STEP_PARAMS[step][StepParam.NEXT_STEP]
+
+    raise UnknownInputStep('Unknown input step')
+
+
+def _get_msg_header(step: InputStep) -> str:
+    if step in _STEP_PARAMS:
+        return _STEP_PARAMS[step][StepParam.MSG_HEADER]
+
+    raise UnknownInputStep('Unknown input step')
+
+
+def _get_step_by_msg_header(header: str) -> InputStep:
+    for step in _STEP_PARAMS:
+        if _STEP_PARAMS[step][StepParam.MSG_HEADER] == header:
+            return step
+
+    raise UnknownInputStep('Unknown step message header')
+
+
+def _get_editing_param_title(step: InputStep) -> str:
+    if step in _STEP_PARAMS:
+        return _STEP_PARAMS[step][StepParam.EDITING_PARAM_TITLE]
+
+    raise UnknownInputStep('Unknown input step')
+
+
+def get_keyboard() -> InlineKeyboardMarkup:
+    return InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton('7', callback_data='7'),
+                InlineKeyboardButton('8', callback_data='8'),
+                InlineKeyboardButton('9', callback_data='9'),
+            ],
+            [
+                InlineKeyboardButton('4', callback_data='4'),
+                InlineKeyboardButton('5', callback_data='5'),
+                InlineKeyboardButton('6', callback_data='6'),
+            ],
+            [
+                InlineKeyboardButton('1', callback_data='1'),
+                InlineKeyboardButton('2', callback_data='2'),
+                InlineKeyboardButton('3', callback_data='3'),
+            ],
+            [
+                InlineKeyboardButton('0', callback_data='0'),
+                InlineKeyboardButton('.', callback_data='.'),
+                InlineKeyboardButton('Ok', callback_data='Ok'),
+            ]
+        ]
+    )
+
+
+def bonds_request_callback(update: Update, context: CallbackContext):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f'{_get_msg_header(InputStep.NOMINAL)}\n{_get_editing_param_title(InputStep.NOMINAL)}:',
+        reply_markup=get_keyboard()
+    )
+
+
+def keyboard_callback(update: Update, context: CallbackContext):
+    query = update.callback_query
+    query.answer()
+
+    msg_lines = query.message.text.splitlines()
+    step = _get_step_by_msg_header(msg_lines[0])
+    editing_line = msg_lines[-1]
+    pressed_button = query.data
+
+    if pressed_button != 'Ok':
+        sep_index = editing_line.find(':')
+        new_value = editing_line[sep_index + 2:] + pressed_button
+        msg_lines[-1] = f'{editing_line[:sep_index]}: {new_value}'
+    else:
+        next_step = _get_next_step(step)
+        if next_step != InputStep.END:
+            msg_lines[0] = _get_msg_header(next_step)
+            msg_lines.append(f'{_get_editing_param_title(next_step)}:')
+        else:
+            msg_lines[0] = f'Доходность к погашению: {_calc(msg_lines)}'
+            query.edit_message_text(text='\n'.join(msg_lines), reply_markup=None)
+
+    replay_text = '\n'.join(msg_lines)
+
+    query.edit_message_text(
+        text=replay_text,
+        reply_markup=get_keyboard()
+    )
+
+
+def _calc(msg_lines: List[str]) -> str:
+    nominal = float(msg_lines[1][msg_lines[1].find(':') + 1:])
+    print(nominal)
+
+    interest_rate = float(msg_lines[2][msg_lines[2].find(':') + 1:]) / 100
+    print(interest_rate)
+
+    d, m, y = map(int, msg_lines[3][msg_lines[3].find(':') + 1:].split('.'))
+    maturity_date = datetime(y, m, d)
+    print(maturity_date)
+
+    market_price = float(msg_lines[4][msg_lines[4].find(':') + 1:])
+    print(market_price)
+
+    today = datetime.now()
+    today = datetime(today.year, today.month, today.day)
+
+    years_to_maturity = (maturity_date - today).days / 365.25
+    print(years_to_maturity)
+
+    profit = (years_to_maturity * nominal * interest_rate + (nominal - market_price))
+    print(profit)
+
+    yield_to_maturity = (profit / years_to_maturity) * 100 / market_price
+
+    return str(round(yield_to_maturity, 2))
--- start_bot.py
+++ start_bot.py
@@ -1,8 +1,9 @@
 import logging
 import re
 
-from telegram.ext import CommandHandler, Dispatcher, Filters, MessageHandler, Updater
+from telegram.ext import CallbackQueryHandler, CommandHandler, Dispatcher, Filters, MessageHandler, Updater
 
+import bonds
 import callbacks
 from commands.start import start
 
@@ -25,6 +26,12 @@ def add_handlers(dispatcher: Dispatcher):
     msg_filter = Filters.text & (~Filters.command) & Filters.regex(echo_re)
     handler = MessageHandler(msg_filter, callbacks.echo_callback)
     dispatcher.add_handler(handler)
+
+    kb_re = re.compile(r'\bbonds\b', re.IGNORECASE)
+    msg_filter = Filters.text & (~Filters.command) & Filters.regex(kb_re)
+    handler = MessageHandler(msg_filter, bonds.bonds_request_callback)
+    dispatcher.add_handler(handler)
+    dispatcher.add_handler(CallbackQueryHandler(bonds.keyboard_callback))
 
 
 def main():
