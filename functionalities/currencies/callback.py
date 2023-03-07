88232fb9702ec1b0c893df94f580a8457fbdac08
Walter-Kovacs <roma.baranov@gmail.com>
2023-03-07 19:49:03 +0300

    Code review.
    1) Constructs request content using xml python module.
    2) Function get_currency_rate_from_xml raises ServiceChanged error when it cannot find the currency tags in the response.
    3) Improve function show_main_currencies.

--- functionalities/currencies/callback.py
+++ functionalities/currencies/callback.py
@@ -1,3 +1,4 @@
+import logging
 from datetime import date
 from xml.etree import ElementTree
 
@@ -7,21 +8,41 @@ from telegram.ext import CallbackContext
 from . import service
 
 
+logger = logging.getLogger(__name__)
+
+
 def show_main_currencies(update: Update, context: CallbackContext):
+    cannot_get_rate_text = 'В данный момент не могу узнать курсы валют :('
+
     service_response = service.send_request(date.today())
     if service_response.status_code == 200:
         xml_root = ElementTree.fromstring(service_response.text)
-        usd = service.get_currency_rate_from_xml('usd', xml_root)
-        eur = service.get_currency_rate_from_xml('eur', xml_root)
-        cny = service.get_currency_rate_from_xml('cny', xml_root)
-        krw = service.get_currency_rate_from_xml('krw', xml_root)
+        try:
+            codes = ['USD', 'EUR', 'CNY', 'KRW']
+            rates = [
+                (
+                    code,
+                    service.get_currency_rate_from_xml(code, xml_root)
+                ) for code in codes
+            ]
 
-        text = f'{usd[0]} USD = {usd[1]} RUB\n' \
-               f'{eur[0]} EUR = {eur[1]} RUB\n' \
-               f'{cny[0]} CNY = {cny[1]} RUB\n' \
-               f'{krw[0]} KRW = {krw[1]} RUB'
+            result = []
+            for code, rate in rates:
+                if rate is not None:
+                    result.append(f'{rate[0]} {code} = {rate[1]} RUB')
+                else:
+                    logger.warning(f'Unknown currency code: {code}')
+
+            if len(result) > 0:
+                text = '\n'.join(result)
+            else:
+                text = cannot_get_rate_text
+
+        except service.ServiceChanged as e:
+            logger.warning(e)
+            text = cannot_get_rate_text
     else:
-        text = 'В данный момент не могу узнать курсы валют :('
+        text = cannot_get_rate_text
 
     context.bot.send_message(
         chat_id=update.effective_chat.id,
--- functionalities/currencies/service.py
+++ functionalities/currencies/service.py
@@ -1,8 +1,43 @@
 import datetime
-from typing import Tuple
+from typing import Tuple, Optional
 from xml.etree import ElementTree
 
 import requests
+
+
+class _RequestContent:
+    def __init__(self):
+        self.root = ElementTree.Element(
+            'soap:Envelope',
+            attrib={
+                'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
+                'xmlns:xsd': 'http://www.w3.org/2001/XMLSchema',
+                'xmlns:soap': 'http://schemas.xmlsoap.org/soap/envelope/',
+            }
+        )
+        body = ElementTree.Element('soap:Body')
+        get_curs_on_date = ElementTree.Element(
+            'GetCursOnDate',
+            attrib={
+                'xmlns': 'http://web.cbr.ru/',
+            }
+        )
+        self.on_date = ElementTree.Element('On_date')
+
+        self.root.append(body)
+        body.append(get_curs_on_date)
+        get_curs_on_date.append(self.on_date)
+
+    def get(self, date: datetime.date) -> bytes:
+        self.on_date.text = f"{date.year}-{str(date.month).rjust(2, '0')}-{str(date.day).rjust(2, '0')}"
+        return ElementTree.tostring(
+            self.root,
+            encoding='utf-8',
+            xml_declaration=True,
+        )
+
+
+_request_content = _RequestContent()
 
 
 def send_request(date: datetime.date) -> requests.Response:
@@ -22,26 +57,16 @@ def send_request(date: datetime.date) -> requests.Response:
     }
     return requests.post(
         url,
-        data=create_request_content(date),
+        data=_request_content.get(date),
         headers=headers,
     )
 
 
-def create_request_content(date: datetime.date) -> str:
-    return f'''<?xml version="1.0" encoding="utf-8"?>
-<soap:Envelope
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-    xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-  <soap:Body>
-    <GetCursOnDate xmlns="http://web.cbr.ru/">
-      <On_date>{date.year}-{str(date.month).rjust(2, '0')}-{str(date.day).rjust(2, '0')}</On_date>
-    </GetCursOnDate>
-  </soap:Body>
-</soap:Envelope>'''
+class ServiceChanged(Exception):
+    pass
 
 
-def get_currency_rate_from_xml(currency_code: str, response_root: ElementTree.Element) -> Tuple[str, str]:
+def get_currency_rate_from_xml(currency_code: str, response_root: ElementTree.Element) -> Optional[Tuple[str, str]]:
     """
     Returns tuple (amount of currency, currency rate)
     Args:
@@ -52,5 +77,8 @@ def get_currency_rate_from_xml(currency_code: str, response_root: ElementTree.El
         (amount of currency, currency rate)
     """
     for el in response_root.iter():
-        if el.tag == 'ValuteCursOnDate' and el.find('VchCode').text.lower() == currency_code.lower():
-            return el.find('Vnom').text, el.find('Vcurs').text
+        try:
+            if el.tag == 'ValuteCursOnDate' and el.find('VchCode').text.lower() == currency_code.lower():
+                return el.find('Vnom').text, el.find('Vcurs').text
+        except AttributeError:
+            raise ServiceChanged('Perhaps the structure of cbr.ru service response has been changed')
