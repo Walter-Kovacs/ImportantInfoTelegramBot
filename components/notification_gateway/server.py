5d6bec824953af4db7e2c311b062af286d929628
Антон Баранов <anton.baranov@corp.mail.ru>
2025-05-03 23:03:01 +0300

    on the way to the distinct thread aiohttp server

--- components/notification_gateway/server.py
+++ components/notification_gateway/server.py
@@ -1,23 +1,31 @@
 import logging
 import traceback
+import queue
+import threading
 from aiohttp import web
+from attrs import define
 from components.notification_gateway.services.telegram_sender import TGSender
 from components.notification_gateway.objects import NotificationSource, NotificationSubscriber
 from telegram import Bot
 
 logger = logging.getLogger(__name__)
 
-PORT = 5054
+web_app_key_queue_to_send = web.AppKey("telegram_sender", queue.Queue)
 
-async def test_handler(request):
+@define
+class QueueMessage:
+    source: NotificationSource
+    message: str 
+
+async def test_handler(request: web.Request):
     logger.info('I catch POST request')
     data = await request.json()
     try:
-        NGServer.telegram_sender.send_to_telegram(
-            NotificationSource(name="hardcode any source", secret=""),
-            f'I catched your message {data}'.encode(),
-            [NotificationSubscriber(tg_id=354628382)],
-        )
+        q: queue.Queue = request.app[web_app_key_queue_to_send]
+        q.put(QueueMessage(
+            source=NotificationSource(name="hardcode any source", secret=""),
+            message=f'I catched your message {data}',
+        ))
         return web.Response(text="Message has been delivered")
     except Exception:
         logging.error(
@@ -28,12 +36,31 @@ async def test_handler(request):
 
 class NGServer:
     telegram_sender: TGSender
+    messages_from_server_queue: queue.Queue
+    PORT: int = 5054
 
-    def start(self, telegram_bot: Bot):
+    async def _queue_handler(self) -> None:
+        msg: QueueMessage = self.messages_from_server_queue.get()
+        await self.telegram_sender.send_to_telegram(
+            msg.source, msg.message, [NotificationSubscriber(tg_id=354628382)],
+        )
+
+
+    def set_telegram_bot(self, telegram_bot: Bot):
         self.telegram_sender = TGSender(bot=telegram_bot)
 
+    def _start_web_server(self, queue_to_send: queue.Queue):
         http_server_app = web.Application()
+        http_server_app[web_app_key_queue_to_send] = queue_to_send 
         http_server_app.add_routes([web.post('/', test_handler)])
 
         logging.info('Starting server in standalone thread')
-        web.run_app(http_server_app, port=PORT)
+        web.run_app(http_server_app, port=self.PORT)
+
+
+    def start(self, port=None) -> None:
+        messages_from_server_queue: queue.Queue = queue.Queue()
+        if port is not None:
+            self.PORT = port
+        threading.Thread(target=lambda: self._start_web_server(messages_from_server_queue)).start()
+
